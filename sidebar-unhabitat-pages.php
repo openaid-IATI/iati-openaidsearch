d8ace0b34a4999b4a65d396521408718ebac5c14
VincentVW <vincentvantwestende@hotmail.com>
2013-04-13 16:17:13 +0200

    Updated popular search items (static like on theme v1)

--- sidebar-unhabitat-pages.php
+++ sidebar-unhabitat-pages.php
@@ -7,10 +7,10 @@
  	<div class="drop-shadow postit page-sidebar-item">
 		<div class="postit-title hneue-light">Popular searches</div>
 		<div class="postit-text">
-			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="#">test1</a></div></div>
-			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="#">test1</a></div></div>
-			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="#">test1</a></div></div>
-			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="#">test1</a></div></div>
-			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="#">test1</a></div></div>
+			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="/projects/?query=Kenya">Kenya</a></div></div>
+			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="/projects/?query=Education">Education</a></div></div>
+			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="/projects/?query=Transparency">Transparency</a></div></div>
+			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="/projects/?query=Settlements">Settlements</a></div></div>
+			<div class="popular-search-item"><div class="pop-search-icon"></div><div><a href="/projects/?query=Strategy">Strategy</a></div></div>
 		</div>
 	</div>
