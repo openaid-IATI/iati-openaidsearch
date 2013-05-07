f01c44ba80899f356ca587f9c4fa553f86013d7b
berjan <berjan@zimmermanzimmerman.nl>
2013-05-07 13:39:48 +0200

    update organisation id

--- constants.php
+++ constants.php
@@ -7,7 +7,7 @@
 define( 'SEARCH_URL', 'http://dev.oipa.openaidsearch.org/api/v2/');
 define( 'SITE_URL', 'http://dev.oipa.openaidsearch.org/');
 define( 'EMPTY_LABEL', 'No information available');
-$_DEFAULT_ORGANISATION_ID = '41120';
+$_DEFAULT_ORGANISATION_ID = 'NL-1';
 $_PER_PAGE = 5;
 $_RELAOD_FILTERS_TIMEOUT = 24*60*60; //24 hours
 
