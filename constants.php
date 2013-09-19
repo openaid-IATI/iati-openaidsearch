4ea7854943c6ee0fbafc7f83c4b14c6386c047cf
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2013-09-19 13:57:39 +0200

    make harmonia work on dev.oipa

--- constants.php
+++ constants.php
@@ -4,8 +4,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-define( 'SEARCH_URL', 'http://oipa.openaidsearch.org/api/v2/');
-define( 'SITE_URL', 'http://oipa.openaidsearch.org/');
+define( 'SEARCH_URL', 'http://dev.oipa.openaidsearch.org/api/v2/');
+define( 'SITE_URL', 'http://dev.oipa.openaidsearch.org/');
 define( 'EMPTY_LABEL', 'No information available');
 $_DEFAULT_ORGANISATION_ID = 'NL-1';
 $_PER_PAGE = 5;
