009abbde4177e1280b9278a092f587d49bea2d06
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2013-10-17 17:52:17 +0200

    id fix

--- constants.php
+++ constants.php
@@ -7,7 +7,7 @@
 define( 'SEARCH_URL', 'http://dev.oipa.openaidsearch.org/api/v3/');
 define( 'SITE_URL', 'http://dev.oipa.openaidsearch.org/');
 define( 'EMPTY_LABEL', 'No information available');
-$_DEFAULT_ORGANISATION_ID = '41120';
+$_DEFAULT_ORGANISATION_ID = 'NL-1';
 $_PER_PAGE = 5;
 define( 'DEFAULT_ORGANISATION_ID', $_DEFAULT_ORGANISATION_ID);
 ?>
