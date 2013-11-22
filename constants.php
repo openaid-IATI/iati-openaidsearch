462a2268ba828cbdd319cb8b29fa0ce6a6f935ae
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2013-11-22 14:17:44 +0100

    added count in filter options

--- constants.php
+++ constants.php
@@ -4,8 +4,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-define( 'SEARCH_URL', 'http://dev.oipa.openaidsearch.org/api/v3/');
-define( 'SITE_URL', 'http://dev.oipa.openaidsearch.org/');
+define( 'SEARCH_URL', 'http://localhost:8000/api/v3/');
+define( 'SITE_URL', 'http://localhost:8000');
 define( 'EMPTY_LABEL', 'No information available');
 
 $_DEFAULT_ORGANISATION_ID = '';
