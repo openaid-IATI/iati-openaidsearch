df3f03785f941bbb4ff0dc3a94bc96333c7c9ad0
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2014-05-27 14:21:05 +0200

    Fix export fix bug

--- constants.php
+++ constants.php
@@ -11,7 +11,7 @@ if (function_exists("site_url")){
 define( 'EMPTY_LABEL', 'No information available');
 define( 'AJAX_PROJECTS_URL', TEMPLATEPATH .'/functions-projects.php');
 
-$_DEFAULT_ORGANISATION_ID = '';
+$_DEFAULT_ORGANISATION_ID = 'NL-1';
 $_PER_PAGE = 5;
 define( 'DEFAULT_ORGANISATION_ID', $_DEFAULT_ORGANISATION_ID);
 ?>
